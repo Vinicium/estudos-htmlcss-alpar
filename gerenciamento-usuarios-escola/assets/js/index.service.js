34feab0b588f04aa6657a9d48650bdfbe2daaf04
Vinicium <143135343+Vinicium@users.noreply.github.com>
2025-04-29 21:00:26 -0300

    fix2

--- gerenciamento-usuarios-escola/assets/js/index.service.js
+++ gerenciamento-usuarios-escola/assets/js/index.service.js
@@ -1,4 +1,3 @@
-const app = angular.module("module", []);
 
 app.service("UsuarioService", function() {
   let usuarios = [
@@ -22,22 +21,4 @@ app.service("UsuarioService", function() {
   };
 });
 
-app.controller("AppController", function($scope, UsuarioService) {
-  $scope.mensagem = "Bem-vindo ao sistema de cadastro escolar";
-  $scope.showAluno = false;
-  $scope.showProfessor = false;
-
-  $scope.usuarios = UsuarioService.listar();
-
-  $scope.adicionarUsuario = function() {
-    if ($scope.novoUsuario.nome && $scope.novoUsuario.tipo) {
-      $scope.novoUsuario.dataCadastro = new Date(); 
-      UsuarioService.adicionar($scope.novoUsuario);
-      $scope.novoUsuario = {};
-    }
-  };
-
-  $scope.removerUsuario = function(index) {
-    UsuarioService.remover(index);
-  };
-});
+ 
